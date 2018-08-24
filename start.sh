07a4b345a1d2a70dff770cddcbb75880e2769437
yzddahaoren <yzddahaoren@163.com>
2018-08-24 19:22:05 +0800

    new

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-nohup ./ngrokd -domain="ngroiiiiik.yzddhr.tk" -httpAddr=":80" -httpsAddr=":443" -tunnelAddr=":8888" &
+nohup ./ngrokd -domain="ngro.yzddhr.tk" -httpAddr=":80" -httpsAddr=":443" -tunnelAddr=":8888" &
